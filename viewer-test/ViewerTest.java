243c734c388105dc5fddab14ac8512125c34338f
chrispope <chrispope@0220a44e-5434-0410-8b8f-031e97fe0784>
2007-07-21 16:28:10 +0000

    Tiling support for VisualBrushes

--- viewer-test/ViewerTest.java
+++ viewer-test/ViewerTest.java
@@ -1,5 +1,6 @@
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.StringBufferInputStream;
 
@@ -19,11 +20,14 @@ import xps.impl.document.jaxb.util.XPSJAXBElementProducer;
 
 public class ViewerTest {
 	private static final String kSimplePath = "<FixedPage xmlns=\"http://schemas.microsoft.com/xps/2005/06\"  Width=\"816\" Height=\"1056\"> <Path Fill=\" #CCCC66\"  Data=\" M 150,0 L 300,150 L 150,300 L 0,150 Z\"  /> </FixedPage>";
-	private static final String kVisualBrushTest = "<FixedPage xmlns=\"http://schemas.microsoft.com/xps/2005/06\"  Width=\"816\" Height=\"1056\"> <Path Fill=\" #CCCC66\"  Data=\" M 150,0 L 300,150 L 150,300 L 0,150 Z\"  /> 			<Path Data=\" M 150,0 L 300,150 L 150,300 L 0,150 Z\" > 			<Path.Fill> 			<VisualBrush Viewbox=\" 0.25,0.25,0.75,0.75\"  Viewport=\" 150,75,50,50\"  			ViewboxUnits=\" Absolute\"  ViewportUnits=\" Absolute\"  TileMode=\" Tile\" > 			<VisualBrush.Visual>     			<Canvas>       			<Path Fill=\" #333399\"  Data=\" M 0.1,0.1 L 0.9,0.1 L 0.9,0.9 L 0.1,0.9 Z\"  /> 			<Path Fill=\" #FFFF00\"  Data=\" M 0.1,0.35 L 0.35,0.1 L 0.6,0.35 L 0.35,0.6 Z\" />   			</Canvas> 			</VisualBrush.Visual> 			</VisualBrush>  			</Path.Fill> 			</Path> 			</FixedPage>";
-
+	private static final String kVisualBrushTest1 = "<FixedPage xmlns=\"http://schemas.microsoft.com/xps/2005/06\"  Width=\"816\" Height=\"1056\"> <Path Fill=\"#CCCC66\" Data=\"M 150,0 L 300,150 L 150,300 L 0,150 Z\" /> 	<Path Data=\"M 150,0 L 300,150 L 150,300 L 0,150 Z\"> 	<Path.Fill> 	<VisualBrush Viewbox=\"0,0,1,1\" Viewport=\"150,75,50,50\" ViewboxUnits=\"Absolute\" ViewportUnits=\"Absolute\" TileMode=\"Tile\"> 	<VisualBrush.Visual> 	<Canvas> 	<Path Fill=\"#333399\" Data=\"M 0.1,0.1 L 0.9,0.1 L 0.9,0.9 L 0.1,0.9 Z\" /> 	<Path Fill=\"#FFFF00\" Data=\"M 0.1,0.35 L 0.35,0.1 L 0.6,0.35 L 0.35,0.6 Z\" /> 	</Canvas> 	</VisualBrush.Visual> 	</VisualBrush> 	</Path.Fill> 	</Path> 			</FixedPage>";
+	private static final String kVisualBrushTest2 = "<FixedPage xmlns=\"http://schemas.microsoft.com/xps/2005/06\"  Width=\"816\" Height=\"1056\"> <Path Fill=\" #CCCC66\"  Data=\" M 150,0 L 300,150 L 150,300 L 0,150 Z\"  /> 	<Path Data=\" M 150,0 L 300,150 L 150,300 L 0,150 Z\" > 	<Path.Fill> 	<VisualBrush Viewbox=\" 0,0,1,1\"              Viewport=\" 125,125,50,50\"   	    ViewboxUnits=\" Absolute\"  ViewportUnits=\" Absolute\"  TileMode=\"Tile\" > 			<VisualBrush.Visual> 				<Canvas> 				<Path Fill=\" #333399\"  Data=\" M 0.1,0.1 L 0.9,0.1 L 0.9,0.9 L 0.1,0.9 Z\"  /> 			<Path Fill=\" #FFFF00\"  Data=\" M 0.1,0.35 L 0.35,0.1 L 0.6,0.35 L 0.35,0.6 Z\" /> 			</Canvas> 			</VisualBrush.Visual> 			</VisualBrush> 				</Path.Fill> 			</Path> 			</FixedPage>";
+	
+	
+	
 	public static void main(String[] args) {
 //		renderSegment(kSimplePath);
-		renderSegment(kVisualBrushTest);
+		renderSegment(kVisualBrushTest2);
 	}
 
 	private static void renderSegment(String wholePage) {
--- viewer/viewer/rendering/brushes/AWTXPSVisualPaintContext.java
+++ viewer/viewer/rendering/brushes/AWTXPSVisualPaintContext.java
@@ -4,10 +4,12 @@ import java.awt.Graphics2D;
 import java.awt.PaintContext;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.Raster;
+import java.util.Vector;
 
 import viewer.rendering.AWTXPSRenderer;
 import viewer.rendering.AWTXPSRenderingUtils;
@@ -16,6 +18,7 @@ import viewer.rendering.ImageLoader;
 import xps.api.XPSError;
 import xps.api.model.document.page.IVisualBrush;
 import xps.impl.XPSVisualElementIterator;
+import xps.impl.document.jaxb.STTileMode;
 
 public class AWTXPSVisualPaintContext extends AWTXPSPaintContext {
 
@@ -25,10 +28,11 @@ public class AWTXPSVisualPaintContext extends AWTXPSPaintContext {
 	private AffineTransform fCurrentTransform;
 	private IVisualBrush fVisualBrush;
 	private ImageLoader fImageLoader;
+	private Rectangle2D fUserBounds;
 
 	public AWTXPSVisualPaintContext(ColorModel cm, PaintContext opacityPaintContext, AffineTransform xform,
 			IVisualBrush visualBrush, FontLoader fontLoader, ImageLoader imageLoader, Rectangle2D locationOfFirstTileToRender,
-			AffineTransform brushTransform) {
+			AffineTransform brushTransform, Rectangle2D userBounds) {
 		
 		super(cm, opacityPaintContext);
 		
@@ -38,6 +42,7 @@ public class AWTXPSVisualPaintContext extends AWTXPSPaintContext {
 		fLocationOfFirstTileToRender = locationOfFirstTileToRender;
 		fCurrentTransform = brushTransform;
 		fVisualBrush = visualBrush;
+		fUserBounds = userBounds;
 	}
 
 	protected Raster getSourceRaster(int x, int y, int w, int h) {
@@ -45,25 +50,20 @@ public class AWTXPSVisualPaintContext extends AWTXPSPaintContext {
 		Graphics2D g2 = (Graphics2D)bi.createGraphics();
 		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 		g2.setClip(0, 0, w, h);
+		AffineTransform totalTransform = calculateTotalTransform(AffineTransform.getTranslateInstance(-x, -y));
+		g2.setTransform(totalTransform);
 		try {
-			AffineTransform totalTransform = calculateTotalTransform(AffineTransform.getTranslateInstance(-x, -y));
-			g2.setTransform(totalTransform);
-		} catch (XPSError e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		
-		//create the renderer that will be used to render the visual brush
-		try {
-			g2.translate(fLocationOfFirstTileToRender.getX(), fLocationOfFirstTileToRender.getY());
-			g2.clipRect(0, 0, (int)Math.ceil(fLocationOfFirstTileToRender.getWidth()), (int)Math.ceil(fLocationOfFirstTileToRender.getHeight()));
-			
-			applyViewbox(g2);
-			
-			AWTXPSRenderer visualBrushRenderer = new AWTXPSRenderer(fFontLoader, fImageLoader, g2);
-			XPSVisualElementIterator iterator = new XPSVisualElementIterator(fVisualBrush);
-			iterator.accept(visualBrushRenderer);
+			if(fVisualBrush.getTileMode() != STTileMode.NONE){
+				TileInfo tilePoints[] = getTilePoints();
+				for (TileInfo info : tilePoints) {
+					Graphics2D singleTileGraphics = (Graphics2D)g2.create();
+					boolean flipX = (fVisualBrush.getTileMode() == STTileMode.FLIP_X || fVisualBrush.getTileMode() == STTileMode.FLIP_XY)  &&  !info.fXEvenRepitition;
+					boolean flipY = (fVisualBrush.getTileMode() == STTileMode.FLIP_Y || fVisualBrush.getTileMode() == STTileMode.FLIP_XY) &&  !info.fYEvenRepitition;
+					renderSingleTile(singleTileGraphics, info.fTileOrigin, flipX, flipY);
+				}
+			} else {
+				renderSingleTile(g2, fLocationOfFirstTileToRender, false, false);
+			}
 		} catch (XPSError e) {
 			//Not  much to do here.
 		}
@@ -71,7 +71,66 @@ public class AWTXPSVisualPaintContext extends AWTXPSPaintContext {
 		return bi.getRaster();
 	}
 	
-	private void applyViewbox(Graphics2D g2) throws XPSError {
+	private void renderSingleTile(Graphics2D singleTileGraphics, Rectangle2D tileBounds, boolean flipX, boolean flipY) throws XPSError {
+		singleTileGraphics.translate(tileBounds.getX(), tileBounds.getY());
+		
+		if(flipX){
+			AffineTransform flipXTransform = AffineTransform.getScaleInstance(-1, 1);
+			flipXTransform.concatenate(AffineTransform.getTranslateInstance(-tileBounds.getWidth(), 0));
+			singleTileGraphics.transform(flipXTransform);
+		} 
+		
+		if(flipY){
+			AffineTransform flipYTransform = AffineTransform.getScaleInstance(1, -1);
+			flipYTransform.concatenate(AffineTransform.getTranslateInstance(0,-tileBounds.getHeight()));
+			singleTileGraphics.transform(flipYTransform);
+		} 
+
+		applyViewboxScaleAndTranslation(singleTileGraphics);
+		AWTXPSRenderer visualBrushRenderer = new AWTXPSRenderer(fFontLoader, fImageLoader, singleTileGraphics);
+		XPSVisualElementIterator iterator = new XPSVisualElementIterator(fVisualBrush);
+		iterator.accept(visualBrushRenderer);
+	}
+
+	private TileInfo[] getTilePoints() {
+		Vector<TileInfo> tilePoints = new Vector<TileInfo>();
+		//find the first X and Y coordinates in page coordinate space that will show a portion of the tile to be shown
+		
+		double startingX = fLocationOfFirstTileToRender.getX();
+		double startingY = fLocationOfFirstTileToRender.getY();
+		boolean evenX = true;
+		boolean evenY = true;
+		
+		//find first x. 
+		while(startingX + fLocationOfFirstTileToRender.getWidth() > fUserBounds.getX()){
+			startingX -= fLocationOfFirstTileToRender.getWidth();
+			evenX = !evenX;
+		}
+		
+		while(startingY + fLocationOfFirstTileToRender.getHeight() > fUserBounds.getY()){
+			startingY -= fLocationOfFirstTileToRender.getHeight();
+			evenY = !evenY;
+		}
+
+		double y = startingY;
+		while(y < fUserBounds.getMaxY()){
+			double x = startingX;	
+			while(x < fUserBounds.getMaxX() ){
+				Rectangle2D tileBounds = new Rectangle2D.Double(x,y, fLocationOfFirstTileToRender.getWidth(), fLocationOfFirstTileToRender.getHeight());
+				tilePoints.add(new TileInfo(tileBounds, evenX, evenY));
+				x += fLocationOfFirstTileToRender.getWidth();
+				evenX = !evenX;
+			}
+			y += fLocationOfFirstTileToRender.getHeight();
+			evenY = ! evenY;
+		}
+		
+		
+		return (TileInfo[]) tilePoints.toArray(new TileInfo[tilePoints.size()]);
+		
+	}
+
+	private void applyViewboxScaleAndTranslation(Graphics2D g2) throws XPSError {
 		//take into account the scaling for viewbox -> viewport
 		Rectangle2D viewport = AWTXPSRenderingUtils.createRectangle(fVisualBrush.getViewport());
 		Rectangle2D viewbox = AWTXPSRenderingUtils.createRectangle(fVisualBrush.getViewbox());
@@ -79,14 +138,27 @@ public class AWTXPSVisualPaintContext extends AWTXPSPaintContext {
 		double yScale = viewport.getHeight() / viewbox.getHeight();
 		
 		g2.transform(AffineTransform.getScaleInstance(xScale, yScale));
-		g2.clip(new Rectangle2D.Double(0, 0,viewbox.getWidth(), viewbox.getHeight()));
 	}
 
-	protected AffineTransform calculateTotalTransform(AffineTransform base) throws XPSError {
+	protected AffineTransform calculateTotalTransform(AffineTransform base) {
 		AffineTransform total = new AffineTransform(base);
 		total.concatenate(fUserToDeviceTransform);
 		total.concatenate(fCurrentTransform);
 		return total;
+	}
+	
+	protected static class TileInfo {
+		public final Rectangle2D fTileOrigin;
+		public final boolean fXEvenRepitition;
+		public final boolean fYEvenRepitition;
+		
+		public TileInfo(Rectangle2D tileOrigin, boolean evenXRepitition, boolean evenYRepitition) {
+			super();
+			fTileOrigin = tileOrigin;
+			fXEvenRepitition = evenXRepitition;
+			fYEvenRepitition = evenYRepitition;
+		}
+		
 	}
 
 }
