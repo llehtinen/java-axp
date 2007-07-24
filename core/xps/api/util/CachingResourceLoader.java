88d40c02f67a5bdd51f358dbf4f8f16531b4768a
chrispope <chrispope@0220a44e-5434-0410-8b8f-031e97fe0784>
2007-07-24 03:17:23 +0000

    Fixed bugs exposed by rendering example 3.6

--- core/xps/api/util/CachingResourceLoader.java
+++ core/xps/api/util/CachingResourceLoader.java
@@ -4,33 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import xps.api.IXPSAccess;
-import xps.api.IXPSFileAccess;
 import xps.api.XPSError;
-import xps.api.model.document.IDocumentReference;
 
 public abstract class CachingResourceLoader<T> {
-	private IXPSFileAccess fFileAccess;
-	private IDocumentReference fDocument;
 	private Map<String, T> fCache = new HashMap<String, T>();
 
-	public CachingResourceLoader(IDocumentReference docRef, IXPSAccess access) {
-		fFileAccess = access.getFileAccess();
-		fDocument = docRef;
+	public CachingResourceLoader() {
 	}
 
-	public IDocumentReference getDocument() {
-		return fDocument;
-	}
-
-	public IXPSFileAccess getFileAccess() {
-		return fFileAccess;
-	}
-	
 	public T load(String uri) throws XPSError {
 		T t = fCache .get(uri);
 		if(t == null){
 			try {
-				t = loadData(uri);
+				t = loadResource(uri);
 				fCache.put(uri, t);
 			}catch  (Exception e) {
 				throw new XPSError(e);		
@@ -41,7 +27,7 @@ public abstract class CachingResourceLoader<T> {
 	}
 	
 	
-	protected abstract T loadData(String uri) throws Exception;
+	protected abstract T loadResource(String uri) throws Exception;
 	
 	
 
--- viewer/viewer/rendering/ImageLoader.java
+++ viewer/viewer/rendering/ImageLoader.java
@@ -3,19 +3,22 @@ package viewer.rendering;
 import java.awt.image.BufferedImage;
 
 import xps.api.IXPSAccess;
+import xps.api.IXPSFileAccess;
 import xps.api.model.document.IDocumentReference;
 import xps.api.util.CachingResourceLoader;
 
 public class ImageLoader extends CachingResourceLoader<BufferedImage>{
 
-	public ImageLoader(IDocumentReference docRef, IXPSAccess access)  {
-		super(docRef, access);
+	private IXPSFileAccess fFileAccess;
+	private IDocumentReference fDocument;
+
+	public ImageLoader(IDocumentReference docRef, IXPSFileAccess access)  {
+		fFileAccess = access;
+		fDocument = docRef;
 	}
 
 	@Override
-	protected BufferedImage loadData(String uri) throws Exception {
-		return getFileAccess().getImageResource(uri,  getDocument());
+	protected BufferedImage loadResource(String uri) throws Exception {
+		return fFileAccess.getImageResource(uri,  fDocument);
 	}
-
-
 }
