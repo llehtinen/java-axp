58d4c6913e9576eb9b1749e2cf7a65caded39945
chrispope <chrispope@0220a44e-5434-0410-8b8f-031e97fe0784>
2009-12-02 03:22:39 +0000

    Handle empty state

--- scrumzilla/src/com/scrumzilla/client/wave/WaveStateWrapper.java
+++ scrumzilla/src/com/scrumzilla/client/wave/WaveStateWrapper.java
@@ -3,27 +3,45 @@ package com.scrumzilla.client.wave;
 import java.util.HashMap;
 
 public class WaveStateWrapper implements WaveState {
-	private final State fState;
-
-	public WaveStateWrapper(State s){
-		fState = s;
+	public WaveStateWrapper(){
 	}
 
 	public String get(String key) {
-		return fState.get(key);
+		State s = getState();
+		if(s == null){
+			return null; 
+		} else {
+			return s.get(key);	
+		}
 	}
 
 	public String get(String key, String optDefault) {
-		return fState.get(key, optDefault);
+		State s = getState();
+		if(s == null){
+			return null; 
+		} else {
+			return s.get(key, optDefault);	
+		}
 	}
 
 	public Integer getInt(String key) {
-		return fState.getInt(key);
+		State s = getState();
+		if(s == null){
+			return null; 
+		} else {
+			return s.getInt(key);	
+		}
 	}
 
 	public void submitDelta(HashMap<String, String> delta) {
-		fState.submitDelta(delta);
+		State s = getState();
+		if(s != null){
+			s.submitDelta(delta); 
+		}
+	}
 
+	private State getState() {
+		return State.getState();
 	}
 
 }
